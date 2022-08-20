d001954257e5bca32ec4447139c9cf93253e0d2c
KevvKo <kklein915@gmail.com>
2022-08-21 01:12:20 +0200

    skip material ui for tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfoCardComponent } from './info-card/info-card.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoCardComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
